419739b35a2e4d802e65c44a33da1e6e90de6843
Enzo-tofa <44833556+Enzo-tofa@users.noreply.github.com>
2021-05-20 17:34:34 +0200

    test

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-function genererCitation(nombre){
-    var motsDebut, motsMilieu, motsFin;
-    var randomDebut, randomMilieu, randomFin;
-    if(nombre===1){
-      motsDebut = ["la vie", "le monde", "le bonheur"];
-      motsMilieu = [" est un mystère qu'il faut vivre", " est un ami qui ne trahit jamais", " c'est comme une bicyclette"];
-      motsFin = [" car nous ne penserons jamais de la même façon"," il faut avancer pour ne pas perdre l'équilibre", " pour les connaître, il faut les aimer"];
-    }
-      else if(nombre===2){
-      motsDebut = ["vu", "quelle que soit", "avec"];
-      motsMilieu = [" la situation actuelle", " la crise de cette fin de siècle", " la conjoncture d'aujourd'hui"];
-      motsFin = [" il faut étudier toutes les solutions"," il serait bon d'imaginer le meilleur", " il faut voir la vie en rose"];
-      }
-
-     else{ console.log("erreur, écrire 1 ou 2");
-    }   
-    randomDebut = motsDebut[Math.floor(Math.random() * motsDebut.length)];
-    randomMilieu = motsMilieu[Math.floor(Math.random() * motsMilieu.length)];
-    randomFin = motsFin[Math.floor(Math.random() * motsFin.length)];
-    var resultat = randomDebut + randomMilieu + randomFin;
-    console.log("résultat : "+resultat);
-    prompt(resultat)
+function generercitation(nombre) {
+  var motsDebut, motsMilieu, motsFin;
+  var randomDebut, randomMilieu, randomFin;
+  if (nombre === 1) {
+    motsDebut = ["la vie", "le monde", "le bonheur","l'amour","la passion"];
+    motsMilieu = [" est un mystère qu'il faut vivre", " est un ami qui ne trahit jamais", " c'est comme une bicyclette"," est intense"," ne se tarit jamais"];
+    motsFin = [" car nous ne penserons jamais de la même façon", " il faut avancer pour ne pas perdre l'équilibre", " pour le connaître, il faut les aimer", " mais c'est de courte durée"," car c'est ce qui nous anime"];
   }
-  function RedirectionJavascript(){
-    document.location.href="http://google.com"; 
+  else if (nombre === 2) {
+    motsDebut = ["vu", "quelle que soit", "avec", "par rapport à ", "en sachant"];
+    motsMilieu = [" la situation actuelle", " la crise de cette fin de siècle", " la conjoncture d'aujourd'hui", " la chute des cryptomonnaies", " la remontée du cumrocket"];
+    motsFin = [" il faut étudier toutes les solutions", " il serait bon d'imaginer le meilleur", " il faut voir la vie en rose", " il faut racheter à la baisse", " il faut rester confiant"];
   }
 
-  function main(){
-    continuer=true;
+  else {
+    console.log("erreur, écrire 1 ou 2");
+  }
+  randomDebut = motsDebut[Math.floor(Math.random() * motsDebut.length)];
+  randomMilieu = motsMilieu[Math.floor(Math.random() * motsMilieu.length)];
+  randomFin = motsFin[Math.floor(Math.random() * motsFin.length)];
+  const resultat = randomDebut + randomMilieu + randomFin;
+  console.log("résultat : " + resultat);
+  prompt(resultat)
+}
 
-    while(continuer){
 
-    var choix = Number(prompt("Taper 1 pour le générateur philosophique ou taper 2 pour le générateur situationelle"));
-    var nombre = Number(prompt("Maintenant, combien de citations souaitez-vous générer ?\n"));
+function main() {
+  continuer = true;
 
-    for(i = 0; i < nombre; i++){
-      genererCitation(choix);
+  while (continuer) {
+
+    const choix = Number(prompt("Taper 1 pour le générateur philosophique ou taper 2 pour le générateur situationelle"));
+    const nombre = Number(prompt("Maintenant, combien de citations souaitez-vous générer ?\n"));
+
+    for (i = 0; i < nombre && i<5; i++) {
+      generercitation(choix);
     }
-    continuer= confirm("Veut tu continuer?")
-    }
-    RedirectionJavascript()
-    }
+    continuer = confirm("Veut tu continuer?")
+  }
+}
